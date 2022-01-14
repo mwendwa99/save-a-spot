a478c0fa7f95e40951804df1f9ce050206c83aa9
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2022-01-14 20:19:03 +0300

    add assets

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,41 +1,14 @@
 import React from 'react';
-import {
-    Typography, Container, Box, Grid, TextField,
-    Avatar
-} from '@mui/material';
-
-import { addDoc, collection, getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+import { Typography, Container, Box, Grid, TextField, Avatar } from '@mui/material';
 
 import Button from '../components/Button';
 import Map from '../components/Map';
-import { useAuth } from '../provider/Authentication';
-import { postToFireStore } from '../api/database';
+import assets from '../assets';
 
-const db = getFirestore();
+import { useAuth } from '../provider/Authentication';
 
 const Home = () => {
     const { signOut } = useAuth();
-
-    React.useEffect(() => {
-        const auth = getAuth();
-        const user = auth.currentUser;
-
-        console.log(user);
-    }, []);
-
-    const postAction = async () => {
-        try {
-            const docRef = await addDoc(collection(db, "users"), {
-                first: "Ada",
-                last: "Lovelace",
-                born: 1815
-            });
-            console.log("Document written with ID: ", docRef.id);
-        } catch (e) {
-            console.error("Error adding document: ", e);
-        }
-    }
 
     const logout = () => {
         signOut();
@@ -49,8 +22,9 @@ const Home = () => {
                 </Typography>
                 <Avatar></Avatar>
                 <Button title="logout" handleAction={() => logout()} />
-                <TextField placeholder="data" />
-                <Button title="post" handleAction={() => postAction()} />
+            </Box>
+            <Box>
+                <img src={assets.map} alt="map-svg" />
             </Box>
             <Grid continer="true">
                 <Map />
